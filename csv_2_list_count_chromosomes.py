a3fb20bbc97b2ee52dfcb730644e5e863dad6b46
maolopez <maurizio.school@gmail.com>
2020-02-26 10:57:26 -0500

    adding chromosomes script

--- csv_2_list_count_chromosomes.py
+++ csv_2_list_count_chromosomes.py
@@ -1,25 +1,25 @@
-#This script extract the genotypes from a CSV file into a list and then calculate the frequency of each genotype.
+#This script extract the chromosomes from a genome's CSV file into a list and then calculate the frequency of each chromosome.
 import csv
 
 #https://thispointer.com/python-find-duplicates-in-a-list-with-frequency-count-index-positions/
-def getDuplicatesWithCount(genotypes):
+def getDuplicatesWithCount(chromosomes):
    ''' Get frequency count of duplicate elements in the given list '''
    dictOfElems = dict()
    # Iterate over each element in list
-   for genotype in genotypes:
+   for chromosome in chromosomes:
       # If element exists in dict then increment its value else add it in dict
-      if genotype in dictOfElems:
-         dictOfElems[genotype] += 1
+      if chromosome in dictOfElems:
+         dictOfElems[chromosome] += 1
       else:
-         dictOfElems[genotype] = 1    
+         dictOfElems[chromosome] = 1    
          # Filter key-value pairs in dictionary. Keep pairs whose value is greater than 1 i.e. only duplicate elements from list.
    dictOfElems = { key:value for key, value in dictOfElems.items() if value > 1}
    # Returns a dict of duplicate elements and thier frequency count
    return dictOfElems
 
 #by my own skill below
-n = 3
-genotypes = []
+n = 1
+chromosomes = []
 genomes = []
 total = 0
 filename = 'human_genome.csv'
@@ -33,21 +33,21 @@ with open(filename, 'r') as my_file:
         my_list = my_list.replace('\n' , ',')
         genomes = my_list.split(',')
         try:
-            #Isolate the genotypes from the genome
+            #Isolate the chromosomes from the gemome
             for genome in genomes:
-               genotypes.append(genomes[n])
+               chromosomes.append(genomes[n])
                n += 4
         except IndexError:
             pass
         continue
 
-#print(genotypes)
-dictOfElems = getDuplicatesWithCount(genotypes)
+#print(chromosomes)
+dictOfElems = getDuplicatesWithCount(chromosomes)
 for key, value in dictOfElems.items():
         total = total + value
 for key, value in dictOfElems.items():
         print('{0:.2f}%'.format((value / total * 100)))
-        print('Genotype', key , ' :: ', value)
+        print('Chromosome', key , ' :: ', value)
 print("The percentages were calculated based in the grand total: " + str(total))
 
 
